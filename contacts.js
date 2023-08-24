904d58747dfa375e5c4d52d287030186ac361375
SSSerg05 <ssserg05@gmail.com>
2023-08-24 23:59:25 +0300

    add nanoid, fix contacts
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- contacts.js
+++ contacts.js
@@ -1,44 +1,84 @@
 // const fs = require('fs').promises;
 import { promises as fs } from 'fs';
-import path from 'path';
+import nanoid from 'nanoid';
 
-const contactsPath = './db/contacts.json';
 
-async function readFile(fileName) {
-  const filePath = path.dirname(fileName)
-  const db = await fs.readFile(contactsPath)
-    .then(data => {
-      //console.log(data.toString())
-      return data.toString();
-    } )
-    .catch(err => console.log(err.message));
-  //console.log(db);
-  return db
+const contactsPath = fs.resolve('db', 'contacts.json');
+
+
+const readData = async (fileName) => {
+  try {
+    const data = await fs.readFile(fileName);
+    return data.toString();   
+
+  } catch (error) {
+    console.log(`Cannot read file or file not found. ${error}`);
+  }
+
+  return null;
 }
 
 
-// TODO: задокументировать каждую функцию
-async function listContacts() {
+
+
+/* List Contacts
+//=======================
+*/
+export const listContacts = async () => {
   // ...твой код. Возвращает массив контактов.
-  return readFile(contactsPath);
+  const data = await readData(contactsPath);
+  console.log(data);
+  return data || null;
 }
 
-function getContactById(contactId) {
+
+// get Contact By Id
+//=======================
+// contactId : string
+//
+export const getContactById = async (contactId) => {
   // ...твой код. Возвращает объект контакта с таким id. Возвращает null если объект с таким id не найден.
-  return null;
+  const data = await readData(contactsPath);
+  const result = data.find(item => item.id === contactId)
+
+  return result || null;
 }
 
-function removeContact(contactId) {
+
+// remove field of DB 
+//=======================
+//  contactId: string
+//
+export const removeContact = async (contactId) => {
   // ...твой код. Возвращает объект удаленного контакта. Возвращает null если объект с таким id не найден.
-  return null;
+  const removeField = getContactById(contactId);
+  
+  const data = await readData(contactsPath);
+  const result = data.filter(item => item !== contactId)
+  await fs.writeFile(contactsPath, result);
+
+  return removeField
 }
 
-function addContact(name, email, phone) {
+
+//  add Contact in DB
+//=======================
+//    name: string,
+//    email: string,
+//    phone: string 
+//
+export const addContact = async (name, email, phone) => {
   // ...твой код. Возвращает объект добавленного контакта. Возвращает null если объект с таким id не найден.
+  const data = await readData(contactsPath);
+  const id = nanoid();
+  const newField = {id, name, email, phone};
+  const result = [...data, newField];
+
+  await fs.writeFile(contactsPath, result);
+
   return null;
 }
 
-listContacts().then(data => console.log(data))
 
 export default {
   listContacts,
